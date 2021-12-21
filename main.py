995c4c574e74e402c2c09f0df06b67fc8ca2660a
ivan <donadelloivan@gmail.com>
2021-12-21 13:15:06 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import pdb
+from src.tree_node_DT import TreeNode
+from src.DT_simulation import biparty_DT
+
+if __name__ == "__main__":
+    root = TreeNode("0", "")
+    n2 = TreeNode("n2", "Low red meat consumption is necessary for a healthy diet.")
+    n3 = TreeNode("n3", "It is really difficult to change diet.")
+    n4 = TreeNode("n4", "I really like the taste of meat.")
+    n5 = TreeNode("n5", "Think about the benefits of reducing red meat.")
+    n6 = TreeNode("n6", "Try to reduce red meat slowly.")
+    n7 = TreeNode("n7", "White meat can be an alternative.")
+    n8 = TreeNode("n8", "Fish is a tasty alternative to meat.")
+    n5.set_utility_opponent(8)
+    n5.set_utility_proponent(2)
+    n6.set_utility_opponent(4)
+    n6.set_utility_proponent(6)
+    n7.set_utility_opponent(0)
+    n7.set_utility_proponent(1)
+    n8.set_utility_opponent(2)
+    n8.set_utility_proponent(4)
+
+    # Creazione albero
+    root.add_child(n2)
+    n2.set_children([n4, n3])
+    n3.set_children([n5, n6])
+    n4.set_children([n7, n8])
+
+    # incapsulamento albero
+    bdt = biparty_DT()
+    bdt.root = root
+    # dict tree l'ho fatta in quanto semplifica il codice di certe funzioni
+    bdt.dict_tree = {0: root, 1: n2, 2: n3, 3: n4, 4: n5, 5: n6, 6: n7, 7: n8}
+
+    # operazioni di decision theory sull'albero
+    bdt.root.compute_chance_decision(is_decision_node=True)
+    bdt.root.propagate_utility("bimaximax")
+
+    # printing
+    bdt.to_pdf("prova")
---
+++ src/tree_node_DT.py
@@ -0,0 +1,113 @@
+from __future__ import annotations
+from operator import attrgetter
+import random
+from typing import List, Type
+import numpy as np
+
+
+def load_samples(filename: str) -> None:
+    samples = np.genfromtxt(filename, delimiter=',', names=True, dtype=None)
+    return samples
+
+
+class TreeNode:
+    delta = 1.0
+
+    def __init__(self, _id, text) -> None:
+        self.labelling: Type[TreeNode] = None
+        self.text: str = text
+        self.height: int = -1
+        self.id: str = _id
+        self.children: list = []
+        self.utility_proponent: int = -1
+        self.utility_opponent: int = -1
+        self.Q_proponent: int = -1
+        self.Q_opponent: int = -1
+        self.is_decision: bool = False # if it is a chance or decision node
+
+    def set_utility_proponent(self, value: int) -> None:
+        self.utility_proponent = value
+
+    def add_child(self, node: Type[TreeNode]) -> None:
+        assert isinstance(node, TreeNode)
+        self.children.append(node)
+
+    def set_children(self, children: list[Type[TreeNode]]) -> None:
+        self.children = children
+
+    def set_utility_opponent(self, utility_opponent: int) -> None:
+        self.utility_opponent = utility_opponent
+
+    def __str__(self) -> str:
+        node_type = "Decision" if self.is_decision is True else "Chance"
+        base_str = f"{node_type} node {self.id} - {self.height}:\n{self.text}\n[{self.Q_proponent}, {self.Q_opponent}]"
+        # for child in self.children:
+        #  base_str += f"\n\t{child.id}: {child.text}"
+        return base_str
+
+    def compute_chance_decision(self, is_decision_node: bool) -> None:
+        self.is_decision = is_decision_node
+        child_type = False if is_decision_node is True else True
+        for child in self.children:
+            child.compute_chance_decision(child_type)
+
+    def isLeaf(self) -> bool:
+        return True if len(self.children) == 0 else False
+
+    def AMax(self) -> list[TreeNode]:
+        """
+        torna i figli del nodo con maxima utilita
+        :return:
+        """
+        output_list = []
+        tmp_list = self.children
+
+        if self.is_decision:
+            key = attrgetter('Q_proponent', 'id')
+        else:
+            key = attrgetter('Q_opponent', 'id')
+
+        tmp_list.sort(key=key, reverse=False)
+        max_util_node = tmp_list[-1]
+        output_list.append(max_util_node)
+        for i in range(0, len(tmp_list) - 1):
+            if self.is_decision:
+               if max_util_node.Q_proponent == tmp_list[i].Q_proponent:
+                   output_list.append(tmp_list[i])
+            else:
+                if max_util_node.Q_opponent == tmp_list[i].Q_opponent:
+                    output_list.append(tmp_list[i])
+        return output_list
+
+    def print_post_order(self) -> None:
+        for n in self.children:
+            n.print_post_order()
+        print(self)
+
+    def propagate_utility(self, policy: str) -> None:
+        for child in self.children:
+            child.propagate_utility(policy=policy)
+        if policy == 'bimaximax':
+            if self.isLeaf(): # the node is a leaf
+                self.Q_proponent = self.utility_proponent
+                self.Q_opponent = self.utility_opponent
+            else:
+                max_utility_child = self.choose_child()
+                self.Q_proponent = self.delta*max_utility_child.Q_proponent
+                self.Q_opponent = self.delta*max_utility_child.Q_opponent
+            if self.is_decision:
+                self.labelling = max_utility_child
+        else:
+            print(f"Policy {policy} not implemented.")
+
+    def choose_child(self, pick_first: bool = False) -> TreeNode:
+        """
+        torna un figlio a casa del nodo con massima/minima utilita
+        :return:
+        """
+        output_list = self.AMax()
+
+        if pick_first:
+            return output_list[0]
+        else:
+            return random.choice(output_list)

2fbb26af7b210b4488bb86399fff16e005d493f7
dragoni <maurodragoni@gmail.com>
2022-01-04 12:01:17 +0100

    Update policies.

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
     # operazioni di decision theory sull'albero
     bdt.root.compute_chance_decision(is_decision_node=True)
-    bdt.root.propagate_utility("harmonic")
+    bdt.root.propagate_utility("bimaximax", -1)
 
     # printing
     bdt.to_pdf("prova")
